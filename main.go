d802a99ada319f917dd5bf25c5e92797e07e8df6
Luqmaan Hakeem <rockgameplayhakeem@gmail.com>
2023-10-08 09:49:16 +0530

    code: Created a simply cli command to check health

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"github.com/urfave/cli/v2"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main(){
+	app := &cli.App{
+		Name: "WebChecker",
+		Usage: "A cli tool to check the website's status",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name: "domain",
+				Aliases: []string{"d"},
+				Usage: "Domain name to check",
+				Required: true,
+			},
+			&cli.StringFlag{
+				Name: "port",
+				Aliases: []string{"p"},
+				Usage: "Port Number to check",
+				Required: false,
+			},
+		},
+		Action: func(c *cli.Context) error{
+			port:= c.String("port")
+			if c.String("port")==""{
+				port="80"
+			}
+			status := Check(c.String("domain"),port)
+			fmt.Println(status)
+			return nil
+		},
+	}
+	err := app.Run(os.Args)
+	if err!= nil{
+		log.Fatal(err)
+	}
+}
